70ed715ec74d03cfcefd288bbe82ac3a32a42315
MiticKa <mitic.natalija96@gmail.com>
2018-12-23 14:46:03 +0100

    web

---
+++ WebApp/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Books;
+
+namespace WebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        DataManager manager = new DataManager();
+        // GET: Book
+        public ActionResult Index(string isbn)
+        {
+            List<List<string>> l = new List<List<string>>();
+            Knjiga k = manager.getKnjiga("666");
+            
+            double o = manager.getOcena(isbn);
+            List<string> komentari = manager.getKomentarKnjiga(isbn);
+           
+
+            List<string> knjiga = new List<string>();
+            knjiga.Add(k.ISBN);
+            knjiga.Add(k.Ime);
+            knjiga.Add(k.Autor);
+            knjiga.Add(k.Datum.ToShortDateString());
+            knjiga.Add(o.ToString());
+            l.Add(knjiga);
+
+            l.Add(komentari);
+
+
+
+            return View(l);
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Books;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataManager manager = new DataManager();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Knjiga> lista = manager.getKnjige();
+            List<List<string>> k = new List<List<string>>();
+            foreach (Knjiga kn in lista)
+            {
+                List<string> l=new List<string>();
+                l.Add(kn.ISBN);
+                l.Add( kn.Ime);
+                l.Add(kn.Autor);
+                l.Add(kn.Datum.ToShortDateString());
+
+                k.Add(l);
+             
+            }
+            
+            return View(k);
+        }
+    }
+}
